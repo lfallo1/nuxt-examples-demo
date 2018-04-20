30ed526ed5206f5e011605f1393be7263f4b9475
Lance Fallon <fallon.lance@gmail.com>
2018-04-20 10:26:32 -0400

    context

--- store/index.js
+++ store/index.js
@@ -18,7 +18,7 @@ export const mutations = {
 
 export const actions = {
 
-  login({commit}, user) {
+  login({commit, error}, user) {
     commit('setLoading', true);
     if (user.username && user.password) {
       axios.post(`http://localhost:8080/oauth/token?username=${user.username}&password=${user.password}&grant_type=password&client_secret=secret&client_id=trustedclient`, null, {headers: {Authorization: 'Basic dHJ1c3RlZGNsaWVudDpzZWNyZXQ='}})
@@ -26,16 +26,17 @@ export const actions = {
           commit('setLoading', false);
           commit('setAuth', response.data);
           this.app.router.push('/member');
-          Vue.toasted.success('You have been logged in', {icon: 'check_circle'}).goAway(3500);
+          Vue.toasted.success('You have been logged in', {icon: 'check_circle', position: 'bottom-right'}).goAway(3500);
         })
         .catch(err => {
           commit('setLoading', false);
-          Vue.toasted.error('Invalid username / password', {icon: 'warning'}).goAway(3500);
+          Vue.toasted.error('Invalid username / password', {icon: 'warning', position: 'bottom-right'}).goAway(3500);
+          window.$nuxt.error({statusCode: 500, message: 'BAD MOVE HORACE'}) //this.$nuxt.error inside components
         });
       return;
     }
     commit('setLoading', false);
-    Vue.toasted.error('Invalid username / password', {icon: 'warning'}).goAway(3500);
+    Vue.toasted.error('Invalid username / password', {icon: 'warning', position: 'bottom-right'}).goAway(3500);
   },
 
   logout({commit, state}) {
@@ -43,12 +44,12 @@ export const actions = {
     axios.post(`http://localhost:8080/logout`, null, {headers: {Authorization: `Bearer ${state.auth.access_token}`}})
       .then(() => {
         commit('setAuth', null);
-        Vue.toasted.info('You have been logged out', {icon: 'highlight_off'}).goAway(3500);
+        Vue.toasted.info('You have been logged out', {icon: 'highlight_off', position: 'bottom-right'}).goAway(3500);
         this.app.router.push('/');
       })
       .catch(err => {
         commit('setAuth', null);
-        Vue.toasted.info('You have been logged out', {icon: 'highlight_off'}).goAway(3500);
+        Vue.toasted.info('You have been logged out', {icon: 'highlight_off', position: 'bottom-right'}).goAway(3500);
         this.app.router.push('/');
       });
   },
